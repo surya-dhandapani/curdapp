5c3f696a552f3fb58f98a46f3414db3eb094e45d
surya-dhandapani <tom.surya@hotmail.com>
2023-09-25 17:56:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Sidebar from "./component/sidebar"
+import Dashboard from "./component/Dashboard"
+function App() {
+  return (
+    <>
+      <div id="wrapper">
+        <Sidebar />
+        <Dashboard />
+      </div>
+
+    </>
+  )
+}
+
+export default App
